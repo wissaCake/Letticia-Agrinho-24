4f16e50b78a111ca70f6838efacb6ffc8d25d6c2
Leletti <111429985+wissaCake@users.noreply.github.com>
2024-08-02 09:13:50 -0300

    Add files via upload

---
+++ Letticia Agrinho 2024/main.js
@@ -0,0 +1,15 @@
+let nomeUsuário = ``;
+let elemento = document.querySelector(`#nome-usuário`);
+
+
+While(nome-usuário == ``){
+    nomeUsuário = prompt(`Qual o seu nome?`);
+
+}
+
+if (nomeUsuário == null){
+    elemento.textContent = `Seja muito bem-vindo`
+}else {
+    elemento.textContent = nomeUsúario;
+}
+
